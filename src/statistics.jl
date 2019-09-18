b73430d3547727086f0f95839dbe39fc56037310
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-09-18 09:14:33 +0000

    Snapshot of branch master taken on 2019-09-18T05:14:31.927-04:00 from "https://github.com/UnofficialJuliaMirror/AutoGrad.jl-6710c13c-97f1-543f-91c5-74e8f7d95b35"

--- src/statistics.jl
+++ src/statistics.jl
@@ -1,6 +1,6 @@
-# Completed: 6/13
+# Completed: 8/13
 
-import Statistics: mean, var, std
+import Statistics: mean, var, std, varm, stdm
 
 # TODO:
 # cor
@@ -13,18 +13,28 @@ import Statistics: mean, var, std
 # quantile
 # quantile! overwrites.
 # std done.
-# stdm
+# stdm done.
 # var done.
-# varm
+# varm done.
 
-@primitive mean(x;d...),dy  (dy.*one.(x).*length(dy)./length(x))
-@primitive mean(f::typeof(abs),x;d...),dy   nothing  (dy.*sign.(x).*length(dy)./length(x))
-@primitive mean(f::typeof(abs2),x;d...),dy  nothing  (dy.*(2x).*length(dy)./length(x))
-
-function var(x::Value; dims=:, mean=mean(x, dims=dims), corrected=true)
-    s = sum(abs2, x .- mean, dims=dims)
-    a = length(x) ÷ length(s) 
-    corrected ? s ./ (a-1) : s ./ a  
-end
+@primitive mean(x;d...),dy  (dy.*one.(x)./(length(x)÷length(dy)))
+@primitive mean(f::typeof(abs),x;d...),dy   nothing  (dy.*sign.(x)./(length(x)÷length(dy)))
+@primitive mean(f::typeof(abs2),x;d...),dy  nothing  (dy.*(2x)./(length(x)÷length(dy)))
 
 std(x::Value, args...; kws...) = sqrt.(var(x, args...; kws...))
+stdm(x::Value, args...; kws...) = sqrt.(varm(x, args...; kws...))
+
+var(x::Value; corrected::Bool=true, mean=nothing, dims=:)=_varm(x, something(mean, Statistics.mean(x,dims=dims)); corrected=corrected, dims=dims)
+varm(x::Value, m; corrected::Bool=true, dims=:)=_varm(x, m; corrected=corrected, dims=dims)
+
+function _varm(x, m; corrected::Bool=true, dims=:)
+    s = sum(abs2, x .- m; dims=dims)
+    r = length(x) ÷ length(s) - Int(corrected)
+    s ./ r
+end
+
+# I don't think making _varm a primitive improves performance, replaces a single sum op with a single _varm op.
+# @primitive _varm(x, m; corrected::Bool=true, dims=:),dy  (dy.*(x.-m).*(2/(length(x)÷length(dy)-Int(corrected))))  nothing
+# @primitive sum(x;d...),dy  (dy.*one.(x))
+# @primitive sum(f::typeof(abs),x;d...),dy   nothing  (dy.*sign.(x))
+# @primitive sum(f::typeof(abs2),x;d...),dy  nothing  (dy.*(2x))
